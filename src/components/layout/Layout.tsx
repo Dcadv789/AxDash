517892233126e99d62b065bf545aa7339e6306ef
Dcadv789 <dc.advisors23@gmail.com>
2025-05-30 17:24:49 -0300

    Commit from Bolt to GitHub

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,18 +10,19 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className={`h-screen flex justify-center ${theme === 'dark' ? 'bg-[#000000]' : 'bg-gray-100'}`}>
       <div className="max-w-[1920px] w-full px-6">
-        <div className="flex relative h-full pt-4">
+        <div className="flex relative h-full pt-6">
           <Sidebar />
           
-          <div className={`flex-grow flex flex-col transition-all duration-300 ${isSidebarCollapsed ? 'ml-28' : 'ml-64'} pt-[2px]`}>
-            <div className="fixed top-4 z-10" style={{ 
-              left: `calc(${isSidebarCollapsed ? '7.5rem' : '17rem'} + 1.5rem)`,
-              right: '1.5rem'
+          <div className={`flex-grow flex flex-col transition-all duration-300 ${isSidebarCollapsed ? 'ml-28' : 'ml-64'}`}>
+            <div className="fixed z-10" style={{ 
+              left: `calc(${isSidebarCollapsed ? '7rem' : '16rem'} + 1.5rem)`,
+              right: '1.5rem',
+              top: '1.5rem'
             }}>
               <Topbar />
             </div>
             
-            <main className="mt-20 flex-1 overflow-auto">
+            <main className="mt-24 flex-1 overflow-auto">
               {children}
             </main>
           </div>
